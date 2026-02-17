a1cd56c1c5b37e1f22c852b19759a0846efcda55
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-17 16:48:41 +0000

    Update noisepan to v0.2.1

--- Formula/noisepan.rb
+++ Formula/noisepan.rb
@@ -4,28 +4,28 @@
 class Noisepan < Formula
   desc "Extract signal from noisy information streams"
   homepage "https://github.com/ppiankov/noisepan"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.0/noisepan_0.2.0_darwin_arm64.tar.gz"
-      sha256 "73d67ec293773a0b9c7a1169efeec7c15009625eff8be6534f48f831ebf3808c"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_darwin_arm64.tar.gz"
+      sha256 "697a072b3666ccee07706c03466158d2c3761129db710164384cfb26a80998b4"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.0/noisepan_0.2.0_darwin_amd64.tar.gz"
-      sha256 "b68b86c2752e46a792f25711a7058ead0e7232e7e260107848c95667846618b4"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_darwin_amd64.tar.gz"
+      sha256 "dbc0710ff5e9bbbb2f68823dce8e7b3ed2a578f4650e628b15fe483b58c5d9b0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.0/noisepan_0.2.0_linux_arm64.tar.gz"
-      sha256 "e742ba67ea50130682d090543c351d66ed0c8682a00bee98b2fdb4cd1668b542"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_linux_arm64.tar.gz"
+      sha256 "d956a3f6871e8ce3da69fbe0822f33049c56d980351045d78c9cf10315618bc3"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.0/noisepan_0.2.0_linux_amd64.tar.gz"
-      sha256 "8ee612ca1a9a67edd6b525cfcc19580cd453d493c40d6f6b37e0442d035d3b18"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_linux_amd64.tar.gz"
+      sha256 "56de66ce1bf94c54333550263cc4fdaf79b9c31c411f98fe4ffba17640ca713e"
     end
   end
 
