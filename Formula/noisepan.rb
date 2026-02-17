58fb097197c379087fadb239d23c5a3a0200a41d
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-17 10:33:32 +0000

    Update noisepan to v0.1.0

--- Formula/noisepan.rb
+++ Formula/noisepan.rb
@@ -10,22 +10,22 @@ class Noisepan < Formula
   on_macos do
     on_arm do
       url "https://github.com/ppiankov/noisepan/releases/download/v0.1.0/noisepan_0.1.0_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "99e8b0981f298c38cfe1ce951ea7ef27741715f1ba52a19773fc00081ba0b0e0"
     end
     on_intel do
       url "https://github.com/ppiankov/noisepan/releases/download/v0.1.0/noisepan_0.1.0_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "17decf33d2ed4f4100e03d3c6dc2ce9988354671fa6df4e6f471e2f73bf53367"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ppiankov/noisepan/releases/download/v0.1.0/noisepan_0.1.0_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "720abe3775263914dc7f2b9ba8120c6d49f42f9dcd1b647ea0ed1b5528af5df5"
     end
     on_intel do
       url "https://github.com/ppiankov/noisepan/releases/download/v0.1.0/noisepan_0.1.0_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "db0010016918971f1f3df127ddc5962b8f8c55cbd5e7cf27714f420026d5492e"
     end
   end
 
