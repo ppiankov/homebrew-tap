e3e2aa39dc05fa6415312005725638199600f631
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-18 16:50:20 +0000

    Update noisepan to v0.2.2

--- Formula/noisepan.rb
+++ Formula/noisepan.rb
@@ -4,28 +4,28 @@
 class Noisepan < Formula
   desc "Extract signal from noisy information streams"
   homepage "https://github.com/ppiankov/noisepan"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_darwin_arm64.tar.gz"
-      sha256 "697a072b3666ccee07706c03466158d2c3761129db710164384cfb26a80998b4"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_darwin_arm64.tar.gz"
+      sha256 "daf9cc97af096e96c6e353ea6fec29d9738e22e7f5d956473071983d62f2ea43"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_darwin_amd64.tar.gz"
-      sha256 "dbc0710ff5e9bbbb2f68823dce8e7b3ed2a578f4650e628b15fe483b58c5d9b0"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_darwin_amd64.tar.gz"
+      sha256 "e2c7a81c2ca406beae2665251301e54cf48db928ad4b1fccc27615dfb38f8957"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_linux_arm64.tar.gz"
-      sha256 "d956a3f6871e8ce3da69fbe0822f33049c56d980351045d78c9cf10315618bc3"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_linux_arm64.tar.gz"
+      sha256 "792bd55772a6a1daf8592a72e322715579b7f0168a7c95bd599265323481032e"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.1/noisepan_0.2.1_linux_amd64.tar.gz"
-      sha256 "56de66ce1bf94c54333550263cc4fdaf79b9c31c411f98fe4ffba17640ca713e"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_linux_amd64.tar.gz"
+      sha256 "5a9d107d2e8e4ab48a79a1c3a2913d5079f2a8eeaa817e166756db7cb56cc789"
     end
   end
 
