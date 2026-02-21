92c74b1602be8253b0b72ff5d44f383e73806236
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-21 15:42:34 +0000

    Update noisepan to v0.3.0

--- Formula/noisepan.rb
+++ Formula/noisepan.rb
@@ -4,28 +4,28 @@
 class Noisepan < Formula
   desc "Extract signal from noisy information streams"
   homepage "https://github.com/ppiankov/noisepan"
-  version "0.2.2"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_darwin_arm64.tar.gz"
-      sha256 "daf9cc97af096e96c6e353ea6fec29d9738e22e7f5d956473071983d62f2ea43"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.3.0/noisepan_0.3.0_darwin_arm64.tar.gz"
+      sha256 "a88e10bcc72d5f188c22a8cdadc51ca580348390a09d84546e3091e8a4c62092"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_darwin_amd64.tar.gz"
-      sha256 "e2c7a81c2ca406beae2665251301e54cf48db928ad4b1fccc27615dfb38f8957"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.3.0/noisepan_0.3.0_darwin_amd64.tar.gz"
+      sha256 "d6e09302cf42a7ce95b3b665d18ed69f043aedaa0f96960b9efe08fa0ae8f55c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_linux_arm64.tar.gz"
-      sha256 "792bd55772a6a1daf8592a72e322715579b7f0168a7c95bd599265323481032e"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.3.0/noisepan_0.3.0_linux_arm64.tar.gz"
+      sha256 "f20a54d0d914726a69adde8feb3e14c2085b9c55cc121763366d259baa429e37"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.2.2/noisepan_0.2.2_linux_amd64.tar.gz"
-      sha256 "5a9d107d2e8e4ab48a79a1c3a2913d5079f2a8eeaa817e166756db7cb56cc789"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.3.0/noisepan_0.3.0_linux_amd64.tar.gz"
+      sha256 "afa9ff7a338ab79e6ec2bffde3f54fb689bbb9a70fdfce62072daef169e50c31"
     end
   end
 
