0da978b236f625d2ad4468694cc0c7c29d5f8f16
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-22 05:20:03 +0000

    Update entropia to v0.3.0

--- Formula/entropia.rb
+++ Formula/entropia.rb
@@ -4,28 +4,28 @@
 class Entropia < Formula
   desc "Evidence support and decay diagnostics for public claims"
   homepage "https://github.com/ppiankov/entropia"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/entropia/releases/download/v0.2.0/entropia_0.2.0_darwin_arm64.tar.gz"
-      sha256 "75f2839770e22ba4a6830f34a520e2c53a31f82ffd0f0e9436047077dcf01735"
+      url "https://github.com/ppiankov/entropia/releases/download/v0.3.0/entropia_0.3.0_darwin_arm64.tar.gz"
+      sha256 "3cc80da185428986a0f18a09ebff7e8c70ec08641d1af5ee5152390758c0b1bd"
     end
     on_intel do
-      url "https://github.com/ppiankov/entropia/releases/download/v0.2.0/entropia_0.2.0_darwin_amd64.tar.gz"
-      sha256 "5769fbc677fcaba401bee1e6007cc00e4081818d0c8fe00ef972db46597f20c6"
+      url "https://github.com/ppiankov/entropia/releases/download/v0.3.0/entropia_0.3.0_darwin_amd64.tar.gz"
+      sha256 "5c78943f9ae1355f96fb195325e12bbe852470092a1222b16f85402e57e1c545"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/entropia/releases/download/v0.2.0/entropia_0.2.0_linux_arm64.tar.gz"
-      sha256 "436ea71cff7fa591c5c3385a0e142ad75d05d744e0995eda2aedcd109d18321e"
+      url "https://github.com/ppiankov/entropia/releases/download/v0.3.0/entropia_0.3.0_linux_arm64.tar.gz"
+      sha256 "a2420948ead430815e8ef83e75ad6b6964f3afa39916a3e2b58d6eccaf75b8be"
     end
     on_intel do
-      url "https://github.com/ppiankov/entropia/releases/download/v0.2.0/entropia_0.2.0_linux_amd64.tar.gz"
-      sha256 "6cdb8a769e0028107157c8996b8f031f9d7ea3b31a449c8cb2027c5d1a120ab7"
+      url "https://github.com/ppiankov/entropia/releases/download/v0.3.0/entropia_0.3.0_linux_amd64.tar.gz"
+      sha256 "2a95d4f54ef2d74dd13b0b80f4b3705b2ffb873842b261edebe283f265569cec"
     end
   end
 
