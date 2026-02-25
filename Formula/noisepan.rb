c0327b4d8036ea415b873d6484a339ba2481b645
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-25 10:58:41 +0000

    Update noisepan to v0.4.3

--- Formula/noisepan.rb
+++ Formula/noisepan.rb
@@ -4,28 +4,28 @@
 class Noisepan < Formula
   desc "Extract signal from noisy information streams"
   homepage "https://github.com/ppiankov/noisepan"
-  version "0.4.2"
+  version "0.4.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.2/noisepan_0.4.2_darwin_arm64.tar.gz"
-      sha256 "352fedc359b4632648be63e0286a0948da68a3763914666d7d5c56bf970c7ee7"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_darwin_arm64.tar.gz"
+      sha256 "573ec047dad1319daf1523fa122064372ee44e3fdf8402cb7b180e90dd457e17"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.2/noisepan_0.4.2_darwin_amd64.tar.gz"
-      sha256 "2596722e88acf626176ed11822163ed3b1b853c4bbb636f7a4217092b533f46b"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_darwin_amd64.tar.gz"
+      sha256 "2fb8ee3669a549aa6057e14048f218277dff54e2c317398ba29bdb0c0d96b0e4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.2/noisepan_0.4.2_linux_arm64.tar.gz"
-      sha256 "d7926f148d996dd581cce805e04b55f2c52cc1cc187f3fe6527667d101bed46b"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_linux_arm64.tar.gz"
+      sha256 "f6b7e38435d3fa1548ce1c3186507cebba0e02c532ba9e75e62148e652c730ce"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.2/noisepan_0.4.2_linux_amd64.tar.gz"
-      sha256 "0d148e20a8b45f4838f4ddc91d355098d46bd1823beffc85ac5c5028765c0429"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_linux_amd64.tar.gz"
+      sha256 "0f238c870836f6eabe9b0df429d4e17127eedcd503e00d4935fffe187a17978e"
     end
   end
 
