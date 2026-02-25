138ded815bba07d4dd5f3789969c272ca8c0ff2a
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-25 08:48:34 +0000

    Update noisepan to v0.4.1

--- Formula/noisepan.rb
+++ Formula/noisepan.rb
@@ -4,28 +4,28 @@
 class Noisepan < Formula
   desc "Extract signal from noisy information streams"
   homepage "https://github.com/ppiankov/noisepan"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.0/noisepan_0.4.0_darwin_arm64.tar.gz"
-      sha256 "42d46ab5a51396016605ae44d063c93620dc058ae379102a071cb32d1bd16336"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.1/noisepan_0.4.1_darwin_arm64.tar.gz"
+      sha256 "ddde27a48c5cbd32473acd24490b1e90b72a188285c3bc2e4bc54af36426b2e3"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.0/noisepan_0.4.0_darwin_amd64.tar.gz"
-      sha256 "82b96fb82847a2d1f055081415f0179627203a4b72f64d129d0ba001ca059c42"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.1/noisepan_0.4.1_darwin_amd64.tar.gz"
+      sha256 "37f5c5ffc26adbed3d80dbebeb56c51d3f6519db31766f91d110c014f9624858"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.0/noisepan_0.4.0_linux_arm64.tar.gz"
-      sha256 "2bacae02be7719818b495bc162e20b1b53c901b6b2b7b4e3569525f8a1a183ec"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.1/noisepan_0.4.1_linux_arm64.tar.gz"
+      sha256 "6ca65bf4802dc00697c024f034cdda86bea19be6c55d8227d4135bf7a2a49987"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.0/noisepan_0.4.0_linux_amd64.tar.gz"
-      sha256 "ab9d51d806b009a710dc5af3d5e35a764875b34268d089188b1ac4b547d97981"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.1/noisepan_0.4.1_linux_amd64.tar.gz"
+      sha256 "7a1b1d983416c2f1bdb04fcd6a352560b01c735e5e18062e54a013b8abeebbd1"
     end
   end
 
