c5e75111f2b57222e3de78d86e33e41448806fd4
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-26 19:05:36 +0800

    Revert "fix: remove arm64 arch constraint from pastewatch formula"
    This reverts commit 4e638c98c1a417f6ab359e9e0518fdf26e71f735.

--- Formula/pastewatch.rb
+++ Formula/pastewatch.rb
@@ -8,6 +8,7 @@ class Pastewatch < Formula
   license "MIT"
 
   depends_on :macos
+  depends_on arch: :arm64
 
   url "https://github.com/ppiankov/pastewatch/releases/download/v0.7.2/pastewatch-cli"
   sha256 "73b529949c617d559858495d4a903f890e8ef38f5a4f8bf3a315e8b11d7a77c1"
