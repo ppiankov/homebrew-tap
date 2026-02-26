6f8302c3634c7feed8b299f74587812a4c7b5eac
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-26 10:00:09 +0000

    Update noisepan to v0.5.0

--- Formula/noisepan.rb
+++ Formula/noisepan.rb
@@ -4,28 +4,28 @@
 class Noisepan < Formula
   desc "Extract signal from noisy information streams"
   homepage "https://github.com/ppiankov/noisepan"
-  version "0.4.3"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_darwin_arm64.tar.gz"
-      sha256 "573ec047dad1319daf1523fa122064372ee44e3fdf8402cb7b180e90dd457e17"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.5.0/noisepan_0.5.0_darwin_arm64.tar.gz"
+      sha256 "fcabcf0f159ef981ba9c58557f7306be21b15acc898316617bb8980c4af56d0b"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_darwin_amd64.tar.gz"
-      sha256 "2fb8ee3669a549aa6057e14048f218277dff54e2c317398ba29bdb0c0d96b0e4"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.5.0/noisepan_0.5.0_darwin_amd64.tar.gz"
+      sha256 "a2236ba300a66f983ba2b6c10770291710581596dacd68be3edb2e69b3a5a480"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_linux_arm64.tar.gz"
-      sha256 "f6b7e38435d3fa1548ce1c3186507cebba0e02c532ba9e75e62148e652c730ce"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.5.0/noisepan_0.5.0_linux_arm64.tar.gz"
+      sha256 "399493f984a894d353714a8712a17000b631e081868853b214b4c912a57819a9"
     end
     on_intel do
-      url "https://github.com/ppiankov/noisepan/releases/download/v0.4.3/noisepan_0.4.3_linux_amd64.tar.gz"
-      sha256 "0f238c870836f6eabe9b0df429d4e17127eedcd503e00d4935fffe187a17978e"
+      url "https://github.com/ppiankov/noisepan/releases/download/v0.5.0/noisepan_0.5.0_linux_amd64.tar.gz"
+      sha256 "aa5d8dfba72ec9c55abc1c52fe8e3942cc3d1385fd768090fbb492ffdf7e99e1"
     end
   end
 
