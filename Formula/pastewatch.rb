fa352795ff1d249ca2f7dbcc3deea2d89d7c7508
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-26 16:06:18 +0000

    Update pastewatch to v0.9.2

--- Formula/pastewatch.rb
+++ Formula/pastewatch.rb
@@ -4,14 +4,14 @@
 class Pastewatch < Formula
   desc "Sensitive data scanner — deterministic detection and obfuscation for text content"
   homepage "https://github.com/ppiankov/pastewatch"
-  version "0.9.1"
+  version "0.9.2"
   license "MIT"
 
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/ppiankov/pastewatch/releases/download/v0.9.1/pastewatch-cli"
-  sha256 "c35ca56ce94183d7692816e0fe8e2af4ca03ea6ccf13935e6694348668d0fc09"
+  url "https://github.com/ppiankov/pastewatch/releases/download/v0.9.2/pastewatch-cli"
+  sha256 "195bfdbd2f4f70143e5f9e5956ddf6aa9e19e15857eb915f3874e70ffacecf0d"
 
   def install
     bin.install "pastewatch-cli"
