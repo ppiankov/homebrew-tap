0e1d950a95fafc9a463e947dfb7f640afd0601be
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-03-26 19:47:01 +0800

    Update pastewatch CLI to universal binary (arm64 + x86_64)

--- Formula/pastewatch.rb
+++ Formula/pastewatch.rb
@@ -9,7 +9,7 @@ class Pastewatch < Formula
 
   on_macos do
     url "https://github.com/ppiankov/pastewatch/releases/download/v0.24.1/pastewatch-cli"
-    sha256 "cf75d863beeb282c339a4de9bd431ae56ca42ea7039591e055fc40cd1995d726"
+    sha256 "a3a02c9d213cb738a859f7b086c4b39bde714834f1aa198f4371b3284cba1878"
   end
 
   on_linux do
