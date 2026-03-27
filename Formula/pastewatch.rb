839c79259fb19ca3773f72e04f91c2105a6e27fe
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-03-27 12:26:06 +0800

    Update pastewatch to v0.25.1

--- Formula/pastewatch.rb
+++ Formula/pastewatch.rb
@@ -7,13 +7,26 @@ class Pastewatch < Formula
   version "0.25.1"
   license "MIT"
 
-  depends_on :macos
-  depends_on arch: :arm64
+  on_macos do
+    url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.1/pastewatch-cli"
+    sha256 "d2ef5a3a5225320131c4f4b903a09459c6adcecaa4ae785cc1416e4e3cbad969"
+  end
 
-  url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.1/pastewatch-cli"
-  sha256 "d5c0b9859c7d92311c97297b612a19d1786547e552528a126a46ca2131157ac8"
+  on_linux do
+    on_arm do
+      url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.1/pastewatch-cli-linux-arm64"
+      sha256 "b592966fee15c8dd6ef6f5a198f473ce52e65e3b4c61e8a68848d0245515703e"
+    end
+
+    on_intel do
+      url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.1/pastewatch-cli-linux-amd64"
+      sha256 "4e52870db149eeca40a602f996c5123ec60a519941ad37806dae410f1fb24661"
+    end
+  end
 
   def install
+    downloaded = Dir["pastewatch-cli*"].first || "pastewatch-cli"
+    mv downloaded, "pastewatch-cli" if downloaded != "pastewatch-cli"
     bin.install "pastewatch-cli"
   end
 
