a278c6624e3b27c2fce93b25561b322b9e2e8fc9
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-03-27 09:29:43 +0000

    Update pastewatch to v0.25.4

--- Formula/pastewatch.rb
+++ Formula/pastewatch.rb
@@ -4,18 +4,18 @@
 class Pastewatch < Formula
   desc "Sensitive data scanner — deterministic detection and obfuscation for text content"
   homepage "https://github.com/ppiankov/pastewatch"
-  version "0.25.3"
+  version "0.25.4"
   license "MIT"
 
   on_macos do
-    url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.3/pastewatch-cli"
-    sha256 "12a8368276e612ab95052b59875fe84470710c7b47a266eda5b75a85de39bfe6"
+    url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.4/pastewatch-cli"
+    sha256 "681c6511b0b30b2c7d0b3cfbff797095263d295c5d03cb6dca967ee924201b76"
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.3/pastewatch-cli-linux-amd64"
-      sha256 "d285f0e98a881c3c5bca79d127fee3b9db09f7789313d0d30d9ec70106749d3f"
+      url "https://github.com/ppiankov/pastewatch/releases/download/v0.25.4/pastewatch-cli-linux-amd64"
+      sha256 "6542834b29887b668098abf0da5533526769d7b1d29bdcfd48ceaac2fd696761"
     end
   end
 
