299cce5500d982452467c5a7e67a63577a26f1cd
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-07-19 13:02:35 +0000

    Update pastewatch to v0.33.0

--- Formula/pastewatch.rb
+++ Formula/pastewatch.rb
@@ -9,17 +9,17 @@ class Pastewatch < Formula
 
   on_macos do
     url "https://github.com/ppiankov/pastewatch/releases/download/v0.33.0/pastewatch-cli"
-    sha256 "e042911be85c3f3116e510c0978ff3de55af2000492f266274611baafb3751fc"
+    sha256 "ec57df3c0fb0bb125157e46b788170875a9cd401fb5c74183245796c231d1b96"
   end
 
   on_linux do
     on_intel do
       url "https://github.com/ppiankov/pastewatch/releases/download/v0.33.0/pastewatch-cli-linux-amd64"
-      sha256 "91e5da57da5430fe2c0e1a15466fae99e156f90256a6a0b28312a37a7e2cba42"
+      sha256 "72391929fe7572a125e422bd80cf294e79922ae508095c7505b289e8c1f7f922"
     end
     on_arm do
       url "https://github.com/ppiankov/pastewatch/releases/download/v0.33.0/pastewatch-cli-linux-arm64"
-      sha256 "caad7cda96aa7ecef4ae94ef0ce8007a1210f20efc267a134cc26abe3cb516a4"
+      sha256 "497202ecc2e498a9f4ecd5a8bdcff5ddadb2b240196e4f9c950dd8f11180525b"
     end
   end
 
