0b48e5dd112d375f4062b9d1de1378de3ecbe071
goreleaserbot <goreleaserbot@users.noreply.github.com>
2026-07-30 10:13:43 +0000

    Brew formula update for iamspectre version 0.8.0

--- Formula/iamspectre.rb
+++ Formula/iamspectre.rb
@@ -1,28 +1,28 @@
 class Iamspectre < Formula
   desc "Cross-cloud IAM auditor — finds unused, over-permissioned, and stale identities"
   homepage "https://github.com/ppiankov/iamspectre"
-  version "0.7.3"
+  version "0.8.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/iamspectre/releases/download/v0.7.3/iamspectre_0.7.3_darwin_arm64.tar.gz"
-      sha256 "c529349ee06b1d5a7f7218e1855f8c9fa9bd6ae19a3b4a2b95fb625e9be9955d"
+      url "https://github.com/ppiankov/iamspectre/releases/download/v0.8.0/iamspectre_0.8.0_darwin_arm64.tar.gz"
+      sha256 "1c29bcd9e9895cc5cb6b238de468e88a037f8a0c7b56f2c8d890ce94920b26d0"
     end
     on_intel do
-      url "https://github.com/ppiankov/iamspectre/releases/download/v0.7.3/iamspectre_0.7.3_darwin_amd64.tar.gz"
-      sha256 "804042ddefc9aaaa3d5eb8231a3418020d06ec877efc3eaf1906c2262a35d9f3"
+      url "https://github.com/ppiankov/iamspectre/releases/download/v0.8.0/iamspectre_0.8.0_darwin_amd64.tar.gz"
+      sha256 "e2c8f41ace2dd2e205b8841b10e4e6dcb5021c2b637f43f05804b1eaa1e4ceca"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/iamspectre/releases/download/v0.7.3/iamspectre_0.7.3_linux_arm64.tar.gz"
-      sha256 "1348cba58a7c3beaa8221659232f80a675e14c9eacea69443078c21f26da8550"
+      url "https://github.com/ppiankov/iamspectre/releases/download/v0.8.0/iamspectre_0.8.0_linux_arm64.tar.gz"
+      sha256 "54cff9549c8a3bad083dedfac3970eadba9663496fa494bd1c450fafe2b9dcb2"
     end
     on_intel do
-      url "https://github.com/ppiankov/iamspectre/releases/download/v0.7.3/iamspectre_0.7.3_linux_amd64.tar.gz"
-      sha256 "4752cfeb0e9a3d0f5bc8f63d1078e147f6f6f0bfc517c4d607828b2813f8f615"
+      url "https://github.com/ppiankov/iamspectre/releases/download/v0.8.0/iamspectre_0.8.0_linux_amd64.tar.gz"
+      sha256 "d7198f99f9036e5be01a52498c8013d4f74e171b02f44183d1cb2e5406d9e473"
     end
   end
 
